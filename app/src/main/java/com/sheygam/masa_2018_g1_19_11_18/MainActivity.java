319fe394e0144b549db1f4798b23816c04e41842
Gregory Sheygam <sheygam@gmail.com>
2018-11-22 09:43:01 +0200

    added calc mini logic

--- app/src/main/java/com/sheygam/masa_2018_g1_19_11_18/MainActivity.java
+++ app/src/main/java/com/sheygam/masa_2018_g1_19_11_18/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     private TextView myTxt;
+    private String operand;
+    private int oper = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +21,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button oneBtn = findViewById(R.id.one_btn);
         Button twoBtn = findViewById(R.id.two_btn);
+        Button plusBtn = findViewById(R.id.plus_btn);
+        Button cancelBtn = findViewById(R.id.cancel_btn);
+        Button equalsBtn = findViewById(R.id.equals_btn);
+        Button ceBtn = findViewById(R.id.ce_btn);
         myTxt = findViewById(R.id.my_txt);
 
         oneBtn.setOnClickListener(this);
         twoBtn.setOnClickListener(this);
+        plusBtn.setOnClickListener(this);
+        equalsBtn.setOnClickListener(this);
+        cancelBtn.setOnClickListener(this);
+        ceBtn.setOnClickListener(this);
 
 
     }
@@ -30,15 +40,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if(v.getId() == R.id.one_btn) {
-//            Toast.makeText(this, "MyBtn click", Toast.LENGTH_SHORT).show();
-            String str = String.valueOf(myTxt.getText());
-            str += "1";
-            myTxt.setText(str);
-//            int res = 1+1;
-//            myTxt.setText(String.valueOf(res));
-
+            myTxt.setText(myTxt.getText().toString() + 1);
         }else if(v.getId() == R.id.two_btn){
-//            Toast.makeText(this, "Second btn clicked!", Toast.LENGTH_SHORT).show();
+            myTxt.setText(myTxt.getText().toString() + 2);
+        }else if(v.getId() == R.id.equals_btn){
+            int a = Integer.parseInt(operand);
+            int b = Integer.parseInt(myTxt.getText().toString());
+            if(oper == 1){
+                myTxt.setText((a + b)+"");
+            }
+            oper = -1;
+            operand = null;
+        }else if(v.getId() == R.id.cancel_btn){
+            operand = null;
+            oper = -1;
+            myTxt.setText("");
+        }else if(v.getId() == R.id.plus_btn){
+            oper = 1;
+            operand = String.valueOf(myTxt.getText());
+            myTxt.setText("");
+        }else if(v.getId() == R.id.ce_btn){
+            if(myTxt.getHint().toString().isEmpty()){
+                myTxt.setHint("0");
+            }else {
+                myTxt.setHint("");
+                myTxt.setText("");
+                operand = null;
+                oper = -1;
+            }
         }
     }
 }
